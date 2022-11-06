f282fedecdf8dd164ce9188bcb7f3956b7a2edeb
Wafi <wafiafdi@gmail.com>
2022-11-06 14:44:05 +0700

    Final build

--- todo.js
+++ todo.js
@@ -6,14 +6,23 @@ while (input !== 'quit' && input !=='q'){
         console.log('---------------');
         console.log('TO DO LIST : ');
         for(let i = 0;i < todo.length; i++){
-            console.log(`${i+1}: ${todo[i]}`)
+            console.log(`${i+1}: ${todo[i]}`);
         }
         console.log('---------------');
     } else if (input === 'new'){
         const newTodo = prompt('Ok, what is new to do');
         todo.push(newTodo);
-        console.log(`${newTodo} added to list`)
-
+        console.log(`${newTodo} added to list`);
+    } else if (input === 'delete' || input === 'd') {
+        const IndexDelete = parseInt(prompt('Insert Index To Delete'));
+        if(!Number.isNaN(IndexDelete)){ 
+            const deleted = todo.splice(IndexDelete-1, 1);
+            console.log(`${deleted[0]} is deleted`);
+        }
+        else {
+            console.log('Unknown Index');
+        }
     }
+    
 }
-console.log("OK QUIT THE APP!")
+console.log("OK QUIT THE APP!");
